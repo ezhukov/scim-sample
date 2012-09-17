32fdc0d496454815cb7f2c9b1238d35f1aa65041
eugene <eugene.zhukov@f-secure.com>
2012-09-17 11:04:22 +0300

    table creations

--- src/main/sql/create_tables.sql
+++ src/main/sql/create_tables.sql
@@ -1,6 +1,17 @@
+DROP TABLE users;
+DROP TABLE emails;
+DROP TABLE phoneNumbers;
+DROP TABLE ims;
+DROP TABLE photos;
+DROP TABLE addresses;
+DROP TABLE groups;
+DROP TABLE entitlements;
+DROP TABLE roles;
+DROP TABLE x509Certificates;
+
 CREATE TABLE users (
-    id                integer PRIMARY KEY,
-    username          varchar(20),
+    id                SERIAL8 PRIMARY KEY,
+    username          varchar(20) unique not null,
     formattedName     varchar(255),
     familyName        varchar(70),
     givenName         varchar(70),
@@ -20,14 +31,103 @@ CREATE TABLE users (
     created           timestamp,
     lastModified      timestamp,
     location          varchar(255),
-    version           varchar(10)
+    version           varchar(10),
+    gender            varchar(6)
 );
 
 CREATE TABLE emails (
-    id        integer PRIMARY KEY,
-    value     varchar(50),
+    id        SERIAL PRIMARY KEY,
+    value     varchar(50) unique not null,
     display   varchar(50),
-    primary   boolean,
+    isPrimary boolean,
     type      varchar(20),
-    operation varchar(20)
+    operation varchar(20),
+    userId    bigint not null
 );
+
+CREATE TABLE phoneNumbers (
+    id        SERIAL PRIMARY KEY,
+    value     varchar(50) not null,
+    display   varchar(50),
+    isPrimary boolean,
+    type      varchar(20),
+    operation varchar(20),
+    userId    bigint not null
+);
+
+CREATE TABLE ims (
+    id        SERIAL PRIMARY KEY,
+    value     varchar(50) not null,
+    display   varchar(50),
+    isPrimary boolean,
+    type      varchar(20),
+    operation varchar(20),
+    userId    bigint not null
+);
+
+CREATE TABLE photos (
+    id        SERIAL PRIMARY KEY,
+    value     varchar(50) not null,
+    display   varchar(50),
+    isPrimary boolean,
+    type      varchar(20),
+    operation varchar(20),
+    userId    bigint not null
+);
+
+CREATE TABLE addresses (
+    id            SERIAL PRIMARY KEY,
+    value         varchar(250),
+    display       varchar(50),
+    isPrimary     boolean,
+    type          varchar(20),
+    operation     varchar(20),
+    formatted     varchar(200),
+    streetAddress varchar(100),
+    locality      varchar(50),
+    region        varchar(50),
+    postalCode    varchar(10),
+    country       char(2),
+    userId        bigint not null
+);
+
+CREATE TABLE groups (
+    id        SERIAL PRIMARY KEY,
+    value     varchar(50) not null,
+    display   varchar(50),
+    isPrimary boolean,
+    type      varchar(20),
+    operation varchar(20),
+    userId    bigint not null
+);
+
+CREATE TABLE entitlements (
+    id        SERIAL PRIMARY KEY,
+    value     varchar(50) not null,
+    display   varchar(50),
+    isPrimary boolean,
+    type      varchar(20),
+    operation varchar(20),
+    userId    bigint not null
+);
+
+CREATE TABLE roles (
+    id        integer PRIMARY KEY,
+    value     varchar(50) not null,
+    display   varchar(50),
+    isPrimary boolean,
+    type      varchar(20),
+    operation varchar(20),
+    userId    bigint not null
+);
+
+CREATE TABLE x509Certificates (
+    id        SERIAL PRIMARY KEY,
+    value     varchar(250) not null,
+    display   varchar(50),
+    isPrimary boolean,
+    type      varchar(20),
+    operation varchar(20),
+    userId    bigint not null
+);
+
