09cfb34317dbb793a99184e93fb9c9ea47170307
eugene <eugene.zhukov@f-secure.com>
2012-10-15 11:04:57 +0300

    test improved

--- src/test/java/eugene/zhukov/util/TestRSA.java
+++ src/test/java/eugene/zhukov/util/TestRSA.java
@@ -1,34 +1,84 @@
 package eugene.zhukov.util;
 
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.security.KeyFactory;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 
 import javax.crypto.Cipher;
-import javax.crypto.CipherInputStream;
 import javax.crypto.CipherOutputStream;
 import javax.xml.bind.DatatypeConverter;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import eugene.zhukov.ApplicationContextProvider;
+
+/**
+ * Some useful commands for public/private keys' generation.
+ * 
+ * # generate a 2048-bit RSA private key
+ * $ openssl genrsa -out private_key.pem 2048
+ *
+ * # convert private Key to PKCS#8 format (so Java can read it)
+ * $ openssl pkcs8 -topk8 -inform PEM -outform DER -in private_key.pem \
+ *   -out private_key.der -nocrypt
+ *
+ * # output public key portion in DER format (so Java can read it)
+ * $ openssl rsa -in private_key.pem -pubout -outform DER -out public_key.der
+ * 
+ * @author eugene
+ *
+ */
 public class TestRSA {
 
-	private static PrivateKey getPrivate() throws Exception {
-	    PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(getKeyBytes("private_key.der"));
-	    return KeyFactory.getInstance("RSA").generatePrivate(spec);
+	@BeforeClass
+	public static void initAppContext() throws Exception {
+		new ApplicationContextProvider().setApplicationContext(
+				new ClassPathXmlApplicationContext("springConfiguration.xml"));
 	}
 
-	private static PublicKey getPublic() throws Exception {
-	    X509EncodedKeySpec spec = new X509EncodedKeySpec(getKeyBytes("public_key.der"));
-	    return KeyFactory.getInstance("RSA").generatePublic(spec);
+	@Test
+	public void check() throws Exception {
+		String password = "pa$$word8888889!";
+		long timestamp = System.currentTimeMillis();
+
+	    S token = new S();
+	    token.setPassword(password);
+	    token.setTimestamp(timestamp);
+
+		String encrypted = encrypt(token);
+
+//        System.out.println(encrypted);
+
+		SecureToken decrypted = Utils.decryptToken(encrypted);
+
+		Assert.assertEquals(password, decrypted.getPassword());
+		Assert.assertEquals(timestamp, decrypted.getTimestamp());
+	}
+
+	public static String encrypt(S token) throws Exception {
+		Cipher c = Cipher.getInstance("RSA");
+		X509EncodedKeySpec spec = new X509EncodedKeySpec(getKeyBytes("public_key.der"));
+	    c.init(Cipher.ENCRYPT_MODE, KeyFactory.getInstance("RSA").generatePublic(spec));
+
+	    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream out = new ObjectOutputStream(baos);
+        out.writeObject(token);
+        out.flush();
+
+        byte[] tokenBytes = baos.toByteArray();
+//        System.out.println("Byte array length before encryption: " + tokenBytes.length);
+
+        baos = new ByteArrayOutputStream();
+        CipherOutputStream cout = new CipherOutputStream(baos, c);
+        cout.write(tokenBytes);
+        cout.close();
+
+        return DatatypeConverter.printBase64Binary(baos.toByteArray());
 	}
 
 	private static byte[] getKeyBytes(String key) throws Exception {
@@ -37,51 +87,5 @@ public class TestRSA {
 		is.read(keyBytes);
 		is.close();
 		return keyBytes;
-	}
-
-	@Test
-	public void check() throws Exception {
-	    S token = new S();
-	    token.setPassword("pa$$word8888889!");
-	    token.setTimestamp(System.currentTimeMillis());
-
-		String encrypted = encrypt(token);
-
-//		System.out.println(encrypted);
-
-		S decrypted = decrypt(encrypted);
-
-//		System.out.println(decrypted);
-		Assert.assertEquals("pa$$word8888889!", decrypted.getPassword());
-	}
-	
-	public static String encrypt(S token) throws Exception {
-		Cipher c = Cipher.getInstance("RSA");
-	    c.init(Cipher.ENCRYPT_MODE, getPublic());
-
-	    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ObjectOutputStream out = new ObjectOutputStream(baos);
-        out.writeObject(token);
-        out.flush();
-        
-        byte[] tokenBytes = baos.toByteArray();
-//        System.out.println("Byte array length before encryption: " + tokenBytes.length);
-        
-        baos = new ByteArrayOutputStream();
-        CipherOutputStream cout = new CipherOutputStream(baos, c);
-        cout.write(tokenBytes);
-        cout.close();
-
-        return DatatypeConverter.printBase64Binary(baos.toByteArray());
-	}
-	
-	private static S decrypt(String token) throws Exception {
-		Cipher c = Cipher.getInstance("RSA");
-		c.init(Cipher.DECRYPT_MODE, getPrivate());
-
-		ByteArrayInputStream in2 = new ByteArrayInputStream(DatatypeConverter.parseBase64Binary(token));
-	    ObjectInputStream ois = new ObjectInputStream(new CipherInputStream(in2, c));
-
-	    return (S) ois.readObject();
 	}
 }
